fbef5fe1ce4d493bd85a3a5e72708c9ff6044256
Ilya Atrakhimenok <ilyaatrakhimenok@MacBook-Pro-Ilya.local>
2025-08-10 11:22:34 -0700

    feat: add note

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,12 @@ import Loader from './components/Loader';
 import styles from './App.module.css';
 import { use, Suspense } from 'react';
 import fetchIngredients from './api/burger-api';
-import Modal from './components/Modal';
 
 function AppContent({ ingredientsPromise }: { ingredientsPromise: Promise<any> }) {
   const ingredients = use(ingredientsPromise);
 
   return (
     <>
-      <Modal title="Modal" isOpen={true} onClose={() => { }}>
-        <p>Modal content</p>
-        <div style={{ height: 500, width: 500 }} />
-      </Modal>
       <AppHeader />
       <main >
         <h2 className="text text_type_main-large pt-10 pb-5">Соберите бургер</h2>
@@ -28,6 +23,7 @@ function AppContent({ ingredientsPromise }: { ingredientsPromise: Promise<any> }
   );
 }
 
+//NOTE: С React 17 я знаком, очень интересно попрактиковаться в 18 и 19 версиях
 function App() {
   return (
     <Suspense fallback={<Loader />}>
