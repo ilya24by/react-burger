22fbce4c17d06a31fa9348ec48dc9e0dd16e3f06
Ilya Atrakhimenok <ilyaatrakhimenok@MacBook-Pro-Ilya.local>
2025-08-10 12:27:27 -0700

    feat: order details

--- src/components/OrderDetails/index.tsx
+++ src/components/OrderDetails/index.tsx
@@ -1,15 +1,18 @@
 import { CheckMarkIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import Modal from "../Modal";
 import styles from './index.module.css';
+import doneImage from '../../images/done.png';
 
 const OrderDetails = ({ isOpen, onClose }: { isOpen: boolean, onClose: () => void }) => {
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
-            <p className="text text_type_digits-large">034536</p>
-            <p className="text text_type_main-default">идентификатор заказа</p>
-            <CheckMarkIcon type="primary" />
-            <p className="text text_type_main-default">Ваш заказ начали готовить</p>
-            <p className={["text text_type_main-default", styles.text_details].join(' ')}>Дождитесь готовности на орбитальной станции</p>
+            <div className={styles.order_details_content}>
+                <p className={["text text_type_digits-large mb-8", styles.order_details_numbers].join(' ')}>034536</p>
+                <p className="text text_type_main-medium mb-15">идентификатор заказа</p>
+                <img src={doneImage} alt="check-mark" />
+                <p className="text text_type_main-default mt-15">Ваш заказ начали готовить</p>
+                <p className={["text text_type_main-default mt-2", styles.text_details].join(' ')}>Дождитесь готовности на орбитальной станции</p>
+            </div>
         </Modal>
     );
 };
